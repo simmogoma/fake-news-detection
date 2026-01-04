2b4ff4487b5c808ea21720b28ef57506e4bbb1fe
simmogoma <simmogoma@gmail.com>
2026-01-04 21:31:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ try:
                     confidence = model.decision_function(vec)[0]
                     
                     st.divider()
-                    if prediction[0] == 0:
+                    if prediction[0] == 1:
                         st.success("### ✅ RESULT: THIS NEWS IS REAL")
                         st.write(f"**AI Confidence Score:** {confidence:.2f}")
                     else:
@@ -124,4 +124,5 @@ try:
 
 except Exception as e:
     st.error(f"Initialization Error: {e}")
+
 
